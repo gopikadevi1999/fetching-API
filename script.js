2a6f3350e5e05ba4f146a0626795d4b80acc9229
gopikadevi1999 <gopikadevir1999@gmail.com>
2023-11-29 12:55:40 +0530

    updated

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const cardsContainer = document.getElementById("cardsContainer");
+    const cardsContainer = document.getElementById("cardsContainer");
 
-  // Function to create a card element
-  const createCard = (data) => {
-    const card = document.createElement("div");
-    card.classList.add("col-lg-4", "col-sm-12", "mb-3");
+    // Function to create a card element
+    const createCard = (data) => {
+        const card = document.createElement("div");
+        card.classList.add("col-lg-4", "col-sm-12", "mb-3");
 
-    const cardElement = document.createElement("div");
-    cardElement.classList.add("card");
+        const cardElement = document.createElement("div");
+        cardElement.classList.add("card");
 
-    const cardHeader = document.createElement("div");
-    cardHeader.classList.add("card-header");
-    cardHeader.innerText = data.name.common;
+        const cardHeader = document.createElement("div");
+        cardHeader.classList.add("card-header");
+        cardHeader.innerText = data.name.common;
 
-    const cardBody = document.createElement("div");
-    cardBody.classList.add("card-body");
+        const cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
 
-    // Adding card content
-    const content = `
+        // Adding card content
+        const content = `
             <img src="${data.flags.png}" alt="Flag">
             <p>Capital: ${data.capital}</p>
             <p>Latlng: ${data.latlng.join(", ")}</p>
@@ -25,39 +25,39 @@ document.addEventListener("DOMContentLoaded", () => {
             <p>Country Codes: ${data.cca3}</p>
         `;
 
-    cardBody.innerHTML = content;
+        cardBody.innerHTML = content;
 
-    // Adding button
-    const button = document.createElement("button");
-    button.classList.add("btn", "btn-primary");
-    button.innerText = "Click for Weather";
-    button.addEventListener("click", () => getWeather(data.cca3)); // Assuming alpha2Code is used as the country code
+        // Adding button
+        const button = document.createElement("button");
+        button.classList.add("btn", "btn-primary");
+        button.innerText = "Click for Weather";
+        button.addEventListener("click", () => getWeather(data.cca3)); // Assuming alpha2Code is used as the country code
 
-    // Appending elements to card
-    cardElement.appendChild(cardHeader);
-    cardElement.appendChild(cardBody);
-    cardBody.appendChild(button);
+        // Appending elements to card
+        cardElement.appendChild(cardHeader);
+        cardElement.appendChild(cardBody);
+        cardBody.appendChild(button);
 
-    card.appendChild(cardElement);
-    cardsContainer.appendChild(card);
-  };
+        card.appendChild(cardElement);
+        cardsContainer.appendChild(card);
+    };
 
-  // Function to fetch data from Rest Countries API
-  const fetchCountryData = () => {
-    fetch("https://restcountries.com/v3.1/all")
-      .then((response) => response.json())
-      .then((data) => {
-        data.forEach((country) => createCard(country));
-      })
-      .catch((error) => console.error("Error fetching country data:", error));
-  };
+    // Function to fetch data from Rest Countries API
+    const fetchCountryData = () => {
+        fetch("https://restcountries.com/v3.1/all")
+            .then((response) => response.json())
+            .then((data) => {
+                data.forEach((country) => createCard(country));
+            })
+            .catch((error) => console.error("Error fetching country data:", error));
+    };
 
-  // Function to fetch weather data from OpenWeatherMap API
-  const getWeather = (countryCode) => {
-    const apiKey = "e8007b5e71dcbb10f72e2bac8dbe1e74";
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?country=${countryCode}&appid=${apiKey}`;
+    // Function to fetch weather data from OpenWeatherMap API
+    const getWeather = (countryCode) => {
+        const apiKey = "e8007b5e71dcbb10f72e2bac8dbe1e74";
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?country=${countryCode}&appid=${apiKey}`;
 
-    fetch(apiUrl)
+        fetch(apiUrl)
             .then((response) => response.json())
             .then((weatherData) => {
                 const weatherElement = document.getElementById(`weather-${countryCode}`);
@@ -72,6 +72,6 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
 
-  // Initial fetch when the page loads
-  fetchCountryData();
+    // Initial fetch when the page loads
+    fetchCountryData();
 });
